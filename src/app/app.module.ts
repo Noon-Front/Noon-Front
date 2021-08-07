fd04492e5b369eab3b22f984b2c16ebf7a95615d
Ahmed Magdy <79047391+ahmedelsangek@users.noreply.github.com>
2021-08-07 11:58:20 +0200

    Create Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Navbar2Component } from './navbar2/navbar2.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    Navbar2Component
+    Navbar2Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
