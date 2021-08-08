061b4ee3f02c2499372270659d8de4c8851aade1
Yehya <yehyajr97@gmail.com>
2021-08-08 11:35:11 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
 import { ProductDetailsComponent } from './product-details/product.details-component';
 
 const routes: Routes = [
-  { path: 'product-details', component: ProductDetailsComponent }
+  { path: 'product-details', component: ProductDetailsComponent },
+  {path: 'cart',component: AddToCartComponent}
 ];
 
 @NgModule({
