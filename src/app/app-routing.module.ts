364505a4fb425627d4120a95d988f889057f0de1
Ahmed Magdy <79047391+ahmedelsangek@users.noreply.github.com>
2021-08-09 21:16:44 +0200

    Create main home Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
 import { ElectronicsPageComponent } from './electronics-page/electronics-page.component';
 import { FashionPageComponent } from './fashion-page/fashion-page.component';
 import { HomeCategoryComponent } from './home-category/home-category.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ProductDetailsComponent } from './product-details/product.details-component';
 
 const routes: Routes = [
+  {path:"", redirectTo:"home", pathMatch:"full"},
+  {path:"home", component:HomePageComponent},
   { path: 'product-details', component: ProductDetailsComponent },
   { path: 'fashion', component: FashionPageComponent },
   { path: 'electronics', component: ElectronicsPageComponent },
