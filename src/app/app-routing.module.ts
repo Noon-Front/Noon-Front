d3d829f2f9280a519428b1be9e029510cbe96e70
Mark-abdallah <83455664+Mark-abdallah@users.noreply.github.com>
2021-08-09 21:12:46 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
 import { ElectronicsPageComponent } from './electronics-page/electronics-page.component';
 import { FashionPageComponent } from './fashion-page/fashion-page.component';
+import { HomeCategoryComponent } from './home-category/home-category.component';
 import { ProductDetailsComponent } from './product-details/product.details-component';
 
 const routes: Routes = [
   { path: 'product-details', component: ProductDetailsComponent },
   { path: 'fashion', component: FashionPageComponent },
   { path: 'electronics', component: ElectronicsPageComponent },
+  { path: 'homeCategory', component: HomeCategoryComponent },
   {path: 'cart',component: AddToCartComponent}
 ];
 
