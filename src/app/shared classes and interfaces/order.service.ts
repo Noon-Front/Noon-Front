592f2f218415da4f05f000effda5790a8e7327dc
Yehya <yehyajr97@gmail.com>
2021-08-17 01:34:53 +0200

    price fix

--- src/app/shared classes and interfaces/order.service.ts
+++ src/app/shared classes and interfaces/order.service.ts
@@ -6,29 +6,29 @@ import { Product } from './product';
 })
 export class OrderService {
   cart: Product[] = [];
-  productPrice:any;
-  totalPrice:number=0;
-  counter:number=0;
+  productPrice: any;
+  totalPrice: number = 0;
+  counter: number = 0;
   constructor() {}
 
   addToCart(product: Product) {
     this.cart.push(product);
-    this.productPrice=product.price;
-    this.totalPrice=this.totalPrice+this.productPrice;
-    this.counter=this.counter+1;
-    console.log(this.counter)
+    this.productPrice = product.price;
+    this.totalPrice = this.totalPrice + this.productPrice;
+    this.counter = this.counter + 1;
+    console.log(this.counter);
   }
   removeFromCart(product: Product) {
     this.cart.forEach((value, index) => {
       if (value.id == product.id) {
-        this.cart.splice(index, 1)
-        this.counter=this.counter-1;
-      };
-     
-    });
-    
-    this.productPrice=product.price;
+        this.cart.splice(index, 1);
+        this.counter = this.counter - 1;
+        this.productPrice = product.price;
     this.totalPrice = this.totalPrice - this.productPrice;
-    console.log(this.totalPrice)
+      }
+    });
+
+    
+    console.log(this.totalPrice);
   }
 }
